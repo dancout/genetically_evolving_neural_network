c01c3fe72adb71b61249139a67eb565824ecc0dc
Daniel Couturier <dancout@umich.edu>
2023-08-30 06:18:46 -0400

    Implemented basic GENN constructor

--- lib/genetically_evolving_neural_network.dart
+++ lib/genetically_evolving_neural_network.dart
@@ -1,7 +1,29 @@
 library genetically_evolving_neural_network;
 
+import 'package:genetic_evolution/genetic_evolution.dart';
+import 'package:genetically_evolving_neural_network/services/genn_fitness_service.dart';
+import 'package:genetically_evolving_neural_network/services/genn_gene_service.dart';
+
 /// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class GeneticallyEvolvingNeuralNetwork {
+  GeneticallyEvolvingNeuralNetwork({
+    required this.fitnessService,
+    required this.geneService,
+    required this.config,
+  });
+
+  final GENNFitnessService fitnessService;
+  final GENNGeneService geneService;
+  final GeneticEvolutionConfig config;
+
+  // TODO: Change from run
+  Future<void> run() async {
+    final generation = GeneticEvolution(
+      geneticEvolutionConfig: config,
+      fitnessService: fitnessService,
+      geneService: geneService,
+    );
+
+    final nextGen = await generation.nextGeneration();
+  }
 }
