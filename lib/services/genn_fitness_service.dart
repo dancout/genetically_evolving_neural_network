427bd457518666faa9d83fccc968595624f3dd7c
Daniel Couturier <dancout@umich.edu>
2023-08-30 06:07:10 -0400

    Create genn_fitness_service.dart

---
+++ lib/services/genn_fitness_service.dart
@@ -0,0 +1,4 @@
+import 'package:genetic_evolution/genetic_evolution.dart';
+import 'package:genetically_evolving_neural_network/models/genn_perceptron.dart';
+
+abstract class GENNFitnessService extends FitnessService<GENNPerceptron> {}
