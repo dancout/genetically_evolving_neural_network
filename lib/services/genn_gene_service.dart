5fe043755add81792020acc834d66cda0ae794b8
Daniel Couturier <dancout@umich.edu>
2023-08-31 07:22:19 -0400

    Moved lots of core functionality into GENNGeneService from test file

--- lib/services/genn_gene_service.dart
+++ lib/services/genn_gene_service.dart
@@ -1,4 +1,59 @@
+import 'dart:math';
+
 import 'package:genetic_evolution/genetic_evolution.dart';
 import 'package:genetically_evolving_neural_network/models/genn_perceptron.dart';
 
-abstract class GENNGeneService extends GeneService<GENNPerceptron> {}
+abstract class GENNGeneService extends GeneService<GENNPerceptron> {
+  GENNGeneService({
+    required this.initialNumWeights,
+    Random? random,
+  }) : random = random ?? Random();
+
+  final Random random;
+
+  final int initialNumWeights;
+  int initialLayer = 0;
+
+  @override
+  Gene<GENNPerceptron> randomGene() {
+    return Gene(
+      value: randomPerceptron(
+        layer: initialLayer,
+        numWeights: initialNumWeights,
+      ),
+    );
+  }
+
+  double randomNegOneToPosOne() => (random.nextDouble() * 2) - 1;
+
+  GENNPerceptron randomPerceptron({
+    required int numWeights,
+    required int layer,
+  }) {
+    assert(
+      numWeights > 0,
+      'numWeights must be initialized before generating a random Perceptron.',
+    );
+
+    return GENNPerceptron(
+      // TODO: Should bias be allowed to be negative?
+      bias: random.nextDouble(),
+      threshold: random.nextDouble(),
+      weights: List.generate(numWeights, (index) => randomNegOneToPosOne()),
+      layer: layer,
+    );
+  }
+
+  GENNPerceptron mutatePerceptron({required GENNPerceptron perceptron});
+
+  @override
+  GENNPerceptron mutateValue({GENNPerceptron? value}) {
+    final gennPerceptron = value;
+    if (gennPerceptron == null) {
+      // TODO: Investigate NN dependency - should value be able to be null ever?
+      throw Exception('Cannot mutate null GENNPerceptron.');
+    }
+
+    return mutatePerceptron(perceptron: gennPerceptron);
+  }
+}
