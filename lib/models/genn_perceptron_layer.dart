5a3968f1ec4f410889d95e06968384a555bd658e
Daniel Couturier <dancout@umich.edu>
2023-09-01 09:04:17 -0400

    Added assertion to GENNPerceptronLayer constructor

--- lib/models/genn_perceptron_layer.dart
+++ lib/models/genn_perceptron_layer.dart
@@ -2,9 +2,17 @@ import 'package:genetically_evolving_neural_network/models/genn_perceptron.dart'
 import 'package:neural_network_skeleton/neural_network_skeleton.dart';
 
 class GENNPerceptronLayer extends PerceptronLayer {
-  const GENNPerceptronLayer({
+  GENNPerceptronLayer({
     required this.gennPerceptrons,
-  }) : super(perceptrons: gennPerceptrons);
+  }) : super(perceptrons: gennPerceptrons) {
+    final layer = gennPerceptrons.first.layer;
+    for (var perceptron in gennPerceptrons) {
+      assert(
+        perceptron.layer == layer,
+        'All Perceptrons within PerceptronLayer must have the same layer.',
+      );
+    }
+  }
 
   final List<GENNPerceptron> gennPerceptrons;
 }
