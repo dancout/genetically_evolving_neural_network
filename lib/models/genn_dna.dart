b33db23d6eddae748b1f29d9df25ac5c9a549791
Daniel Couturier <dancout@umich.edu>
2023-09-06 22:18:51 -0400

    Sorting genes within GENNDNA to avoid crossover bug

--- lib/models/genn_dna.dart
+++ lib/models/genn_dna.dart
@@ -3,9 +3,17 @@ import 'package:genetically_evolving_neural_network/models/genn_gene.dart';
 import 'package:genetically_evolving_neural_network/models/genn_perceptron.dart';
 
 class GENNDNA extends DNA<GENNPerceptron> {
-  const GENNDNA({
+  GENNDNA({
     required this.gennGenes,
-  }) : super(genes: gennGenes);
+  }) : super(
+            genes: gennGenes
+              // TODO: This works, but see if there is a more efficient way of
+              /// doing this rather than sorting every time. Potentially by
+              /// adding genes into the correct place within the list when you
+              /// make them (so you don't have to sort later).
+              ..sort(
+                (a, b) => (a.value.layer > b.value.layer) ? 1 : -1,
+              ));
 
   final List<GENNGene> gennGenes;
 
