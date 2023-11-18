1ad29dc0a5f3db61b02e9ce61aa8e215518f0ac5
Daniel Couturier <dancout@umich.edu>
2023-11-18 09:04:27 -0500

    Using config's random for GennGeneService

--- lib/genetically_evolving_neural_network.dart
+++ lib/genetically_evolving_neural_network.dart
@@ -19,6 +19,7 @@ part 'package:genetically_evolving_neural_network/services/genn_entity_service.d
 part 'package:genetically_evolving_neural_network/services/genn_fitness_service.dart';
 part 'package:genetically_evolving_neural_network/services/genn_gene_mutation_service.dart';
 part 'package:genetically_evolving_neural_network/services/genn_gene_service/genn_gene_service.dart';
+part 'package:genetically_evolving_neural_network/services/genn_gene_service/genn_gene_service_helper.dart';
 part 'package:genetically_evolving_neural_network/services/perceptron_layer_mutation_service.dart';
 
 /// Represents a Genetically Evolving Neural Network.
@@ -48,7 +49,10 @@ class GENN extends GeneticEvolution<GENNPerceptron> {
     // Use the geneService passed in if any customizations are necessary.
     final gennGeneService = geneService ??
         // Otherwise, generate one internally.
-        GENNGeneService(numInitialInputs: config.numInitialInputs);
+        GENNGeneService(
+          numInitialInputs: config.numInitialInputs,
+          random: config.random,
+        );
 
     final geneMutationService = GENNGeneMutationService(
       trackMutatedWaves: config.trackMutatedWaves,
