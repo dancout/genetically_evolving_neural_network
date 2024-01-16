d5041f64e5604fe7c4a24f2d592e5fd044cd5911
Daniel Couturier <dancout@umich.edu>
2024-01-16 08:35:46 -0500

    Removed TODO

--- lib/models/genn_dna.dart
+++ lib/models/genn_dna.dart
@@ -1,20 +1,21 @@
 part of 'package:genetically_evolving_neural_network/genetically_evolving_neural_network.dart';
 
+/// An extension of [NeuralNetwork.DNA].
 class GENNDNA extends DNA<GENNPerceptron> {
   GENNDNA({
     required this.gennGenes,
   }) : super(
             genes: gennGenes
-              // TODO: This works, but see if there is a more efficient way of
-              /// doing this rather than sorting every time. Potentially by
-              /// adding genes into the correct place within the list when you
-              /// make them (so you don't have to sort later).
+              // Sort the genes by Perceptron layer for consistency.
+              // NOTE:  This is necessary, otherwise perceptrons may be crossed
+              //        over among different layers, which is unintentional.
               ..sort(
                 (a, b) => (a.value.layer > b.value.layer) ? 1 : -1,
               ));
 
   final List<GENNGene> gennGenes;
 
+  /// Returns a [GENNDNA] object created from the input [DNA].
   factory GENNDNA.fromDNA({required DNA<GENNPerceptron> dna}) {
     final gennGenes = <GENNGene>[];
 
--- lib/models/genn_gene.dart
+++ lib/models/genn_gene.dart
@@ -1,12 +1,14 @@
 part of 'package:genetically_evolving_neural_network/genetically_evolving_neural_network.dart';
 
+/// An extension of [NeuralNetwork.Gene].
 class GENNGene extends Gene<GENNPerceptron> {
   const GENNGene({
     required super.value,
     super.mutatedWaves,
   });
 
-  factory GENNGene.fromGene({required Gene gene}) {
+  /// Returns a [GENNGene] object created from the input [Gene].
+  factory GENNGene.fromGene({required Gene<GENNPerceptron> gene}) {
     return GENNGene(
       value: gene.value,
       mutatedWaves: gene.mutatedWaves,
