bf19ed4b5e92bc2efdec71da4d145f635e2325ca
Daniel Couturier <dancout@umich.edu>
2024-01-21 22:48:17 -0500

    Updated GENNFItnessService
    It now handles the nonZeroBias and takes in a Neural Network instead of DNA.

--- lib/services/genn_fitness_service.dart
+++ lib/services/genn_fitness_service.dart
@@ -1,7 +1,17 @@
 part of 'package:genetically_evolving_neural_network/genetically_evolving_neural_network.dart';
 
+/// An extension of [GeneticEvolution.FitnessService].
 abstract class GENNFitnessService extends FitnessService<GENNPerceptron> {
-  Future<double> gennScoringFunction({required GENNDNA gennDna});
+  /// The internal scoring function used to calculate the fitness score of the
+  /// input [neuralNetwork].
+  ///
+  /// This value is meant to be non-negative.
+  Future<double> gennScoringFunction({
+    required GENNNeuralNetwork neuralNetwork,
+  });
+
+  @override
+  double get nonZeroBias => 0.01;
 
   @override
   Future<double> scoringFunction({required DNA<GENNPerceptron> dna}) {
@@ -9,6 +19,12 @@ abstract class GENNFitnessService extends FitnessService<GENNPerceptron> {
     //        method used within the GeneticEvolution library. It is used so we
     //        can be stricter about using GENNDNA instead of
     //        DNA<GENNPerceptron>.
-    return gennScoringFunction(gennDna: GENNDNA.fromDNA(dna: dna));
+
+    // Declare the NeuralNetwork
+    final neuralNetwork = GENNNeuralNetwork.fromGenes(
+      genes: GENNDNA.fromDNA(dna: dna).genes,
+    );
+
+    return gennScoringFunction(neuralNetwork: neuralNetwork);
   }
 }
