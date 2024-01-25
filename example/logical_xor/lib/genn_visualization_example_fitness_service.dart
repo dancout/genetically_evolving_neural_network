2bd0d152e7d6865f2b6386df4246532cc217289a
Daniel Couturier <dancout@umich.edu>
2024-01-25 08:12:37 -0500

    Create genn_visualization_example_fitness_service.dart

---
+++ example/logical_xor/lib/genn_visualization_example_fitness_service.dart
@@ -0,0 +1,42 @@
+import 'package:genetically_evolving_neural_network/genetically_evolving_neural_network.dart';
+import 'package:logical_xor/genn_visualization_example.dart';
+
+/// Represents a wrapper class that extends [GENNFitnessService] and implements
+/// [GENNVisualizationExample].
+class GENNVisualizationExampleFitnessService extends GENNFitnessService
+    implements GENNVisualizationExample {
+  @override
+  Future<double> gennScoringFunction(
+      {required GENNNeuralNetwork neuralNetwork}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  List<List<double>> getNeuralNetworkGuesses(
+      {required GENNNeuralNetwork neuralNetwork}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  double? get highestPossibleScore => throw UnimplementedError();
+
+  @override
+  List<List<double>> get inputsList => throw UnimplementedError();
+
+  @override
+  List<String> get readableInputList => throw UnimplementedError();
+
+  @override
+  List<String> get readableTargetList => throw UnimplementedError();
+
+  @override
+  double? get targetFitnessScore => throw UnimplementedError();
+
+  @override
+  List<List<double>> get targetOutputsList => throw UnimplementedError();
+
+  @override
+  String convertToReadableString(List<double> valueList) {
+    throw UnimplementedError();
+  }
+}
