dae69ba28445dc2626f1abcd03ca91973aca5b7c
Daniel Couturier <dancout@umich.edu>
2024-02-01 07:42:20 -0500

    Refactored mutatePerceptronLayersWithinEntity to be more efficient
    Instead of recreating an entire GENNNeuralNetwork, we are just recreated the GENNPerceptronLayer that we need.

--- lib/services/genn_entity_service_helper.dart
+++ lib/services/genn_entity_service_helper.dart
@@ -1,14 +1,19 @@
 part of 'package:genetically_evolving_neural_network/genetically_evolving_neural_network.dart';
 
+/// This class is responsible for adding or removing [GENNPerceptron] objects
+/// within a [GENNPerceptronLayer] and adding or removing [GENNPerceptronLayer]
+/// objects from a [GENNEntity].
 class GENNEntityServiceHelper {
-  GENNEntityServiceHelper(
-      // TODO: Double check all the required params here
-      {
-    required this.numberGenerator,
+  /// This class is responsible for adding or removing [GENNPerceptron] objects
+  /// within a [GENNPerceptronLayer] and adding or removing
+  /// [GENNPerceptronLayer] objects from a [GENNEntity].
+  GENNEntityServiceHelper({
+    Random? random,
+    NumberGenerator? numberGenerator,
     required this.layerMutationRate,
     required this.perceptronMutationRate,
     required this.perceptronLayerMutationService,
-  });
+  }) : numberGenerator = numberGenerator ?? NumberGenerator(random: random);
 
   /// Used to generate random numbers and bools.
   final NumberGenerator numberGenerator;
@@ -24,18 +29,15 @@ class GENNEntityServiceHelper {
   /// Used for mutating Perceptron Layers on the Entities.
   late final PerceptronLayerMutationService perceptronLayerMutationService;
 
+  /// Adds or removes a [GENNPerceptronLayer] to the input [child].
   Future<GENNEntity> mutatePerceptronLayersWithinEntity({
     required GENNEntity child,
-    // required double randNumber,
   }) async {
     // TODO: Is the copywith necessary?
     var updatedChild = child.copyWith();
 
     final randNumber = numberGenerator.nextDouble;
-    final gennNN = GENNNeuralNetwork.fromGenes(
-      genes: List<GENNGene>.from(updatedChild.dna.genes),
-    );
-    var numLayers = gennNN.numLayers;
+    final numLayers = updatedChild.maxLayerNum + 1;
 
     // Add or Remove PerceptronLayer from Entity if mutation condition met.
     if (randNumber < layerMutationRate) {
@@ -43,12 +45,19 @@ class GENNEntityServiceHelper {
       // must add to it.
       if (numLayers == 1 || numberGenerator.nextBool) {
         // Randomly pick a layer to duplicate
-        final duplicationLayer = numberGenerator.nextInt(numLayers);
+        final targetLayer = numberGenerator.nextInt(numLayers);
 
-        // Duplicate PerceptronLayer
+        // Declare the PerceptronLayer to duplicate
+        final duplicationLayer = GENNPerceptronLayer(
+            perceptrons: updatedChild.dna.genes
+                .where((gene) => gene.value.layer == targetLayer)
+                .map((gene) => gene.value)
+                .toList());
+
+        // Extract the duplicated PerceptronLayer
         final duplicatedPerceptronLayer =
             perceptronLayerMutationService.duplicatePerceptronLayer(
-          gennPerceptronLayer: gennNN.layers[duplicationLayer],
+          gennPerceptronLayer: duplicationLayer,
         );
 
         // Add PerceptronLayer into Entity
@@ -57,8 +66,6 @@ class GENNEntityServiceHelper {
           entity: updatedChild,
           perceptronLayer: duplicatedPerceptronLayer,
         );
-        // Increment the number of Perceptron Layers
-        numLayers++;
       } else {
         // NOTE:  Cannot remove last layer, hence the -1. This is because the
         //        last layer represents the expected outputs, and that cannot
@@ -71,27 +78,21 @@ class GENNEntityServiceHelper {
           entity: updatedChild,
           targetLayer: targetLayer,
         );
-        // Decrement the number of Perceptron Layers
-        numLayers--;
       }
     }
 
     return updatedChild;
   }
 
+  /// Adds or removes a [GENNPerceptron] to a random [GENNPerceptronLayer]
+  /// within the input [child].
   Future<GENNEntity> mutatePerceptronsWithinLayer({
     required GENNEntity child,
-    // required double randNumber,
   }) async {
     // TODO: Is the copywith necessary?
     var updatedChild = child.copyWith();
 
-    // TODO: EXTRA DOUBLE CHECK ON THIS! I AM CREATING THIS ON THE FLY INSTEAD
-    /// OF USING IT PASSED FORWARD!
-    // final numLayers = updatedChild.maxLayerNum + 1;
-    final gennNN = GENNNeuralNetwork.fromGenes(genes: updatedChild.dna.genes);
-    var numLayers = gennNN.numLayers;
-
+    final numLayers = updatedChild.maxLayerNum + 1;
     final randNumber = numberGenerator.nextDouble;
 
     // Add or Remove a Perceptron from a PerceptronLayer if there is more than
