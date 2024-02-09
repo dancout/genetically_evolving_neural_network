7916162240a91d2d95203b260a1a5d828a98267d
Daniel Couturier <dancout@umich.edu>
2024-02-08 20:02:36 -0500

    Created simple_example main file

--- example/simple_example/lib/main.dart
+++ example/simple_example/lib/main.dart
@@ -1,125 +1,116 @@
 import 'package:flutter/material.dart';
+import 'package:genetically_evolving_neural_network/genetically_evolving_neural_network.dart';
+
+/// The inputs for this Neural Network, from -1 to 1 in increments of 0.1.
+List<double> get inputs => List.generate(10, (index) => index * 0.1) // [0 to 1]
+  ..addAll(List.generate(9, (index) => (index + 1) * -0.1)); // [-0.1 to -1]
+
+/// The scoring function that will be used to evolve entities of a population
+class PositiveNumberFitnessService extends GENNFitnessService {
+  @override
+  Future<double> gennScoringFunction({
+    required GENNNeuralNetwork neuralNetwork,
+  }) async {
+    // Calculate how many correct guesses were made
+    return inputs.fold(0, (previousValue, input) {
+      final guess = neuralNetwork.guess(inputs: [input])[0];
+      // Only add a point if the neural network guesses correctly
+      if ((input > 0 && guess > 0) || (input <= 0 && guess == 0)) {
+        return previousValue + 1;
+      }
+      return previousValue;
+    }).toDouble();
+  }
+}
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+/// This class houses an example app to visually display how the incoming
+/// [VisualizationExampleGENNFitnessService] works.
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  /// Represents the FitnessService used to drive this GENN example.
+  final GENNFitnessService fitnessService = PositiveNumberFitnessService();
+
+  /// The current generation of Neural Networks.
+  GENNGeneration? generation;
+
+  /// The Genetically Evolving Neural Network object.
+  late final GENN genn;
+
+  @override
+  void initState() {
+    // Declare a config with specific mutation rates.
+    final config = GENNGeneticEvolutionConfig(
+      populationSize: 20,
+      numOutputs: 1,
+      mutationRate: 0.1,
+      numInitialInputs: 1,
+      layerMutationRate: 0.25,
+      perceptronMutationRate: 0.4,
     );
-  }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+    // Create the GENN object from the incoming config and fitness service.
+    genn = GENN.create(
+      config: config,
+      fitnessService: fitnessService,
+    );
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+    // Initialize the first generation
+    genn.nextGeneration().then((value) {
+      setState(() {
+        generation = value;
+      });
     });
+
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    final generation = this.generation;
+    if (generation == null) {
+      return const CircularProgressIndicator();
+    }
+
+    return MaterialApp(
+      title: 'GENN Example',
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('generation #: ${generation.wave.toString()}'),
+              Text('top score: '
+                  '${generation.population.topScoringEntity.fitnessScore} '
+                  'out of a possible ${inputs.length}.01'),
+              Text('top scoring entity\'s # of layers: '
+                  '${generation.population.topScoringEntity.maxLayerNum + 1}'),
+              Text('top scoring entity\'s # of perceptrons: '
+                  '${generation.population.topScoringEntity.dna.genes.length}'),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+          label: const Text('Next Generation'),
+          onPressed: () {
+            // Set the next Generation to be displayed
+            genn.nextGeneration().then((value) {
+              setState(() {
+                this.generation = value;
+              });
+            });
+          },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
