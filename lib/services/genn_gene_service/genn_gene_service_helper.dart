ad73daab52fd0b46f0ab60746f5e631864aadcea
Daniel Couturier <dancout@umich.edu>
2024-07-24 07:50:09 -0400

    Updated weights initialization to shrink according to sqrt of numWeights

--- lib/services/genn_gene_service/genn_gene_service_helper.dart
+++ lib/services/genn_gene_service/genn_gene_service_helper.dart
@@ -24,7 +24,6 @@ class GennGeneServiceHelper {
     int selectedOption =
         gennGeneServiceMutationHelper.selectMutationOption(perceptron);
 
-
     // TODO: What I want to do differently, is that we shouldn't have an X% chance that this *gene* will mutate, but maybe we should instead focus on there being a X% chance that *any feature* will mutate.
     /// What this means is that we should run through all possible mutation options, and do an X% chance check against each option.
     /// Steps:
@@ -35,9 +34,8 @@ class GennGeneServiceHelper {
     ///     - Generate a random value
     ///     - If the random value is less than the "originalMutationRate", then call "mutateBasedOnSelectedOption" for that index
     /// NOTE: We might have to update how this is done, because "mutateBasedOnSelectedOption" returns an entire gene, not just that rand value
-    /// 
-    /// OORRRRRRR - We could override the mutateGene call? To not have to deal with *every* gene now having *every* wave show "mutatedWave" or whatever 
-
+    ///
+    /// OORRRRRRR - We could override the mutateGene call? To not have to deal with *every* gene now having *every* wave show "mutatedWave" or whatever
 
     // Return a new GENNPerceptron with its selected mutation
     return gennGeneServiceMutationHelper.mutateBasedOnSelectedOption(
@@ -54,12 +52,18 @@ class GennGeneServiceHelper {
       'numWeights must be greater than 0 when creating a random Perceptron.',
     );
 
+    // According to Tariq Rashid's book, "Make Your Own Neural Network", the
+    // weights should range from -1/sqrt(numWeights) to 1/sqrt(numWeights).
+    final weightShrinkingFactor = sqrt(numWeights);
+
     return GENNPerceptron(
       bias: numberGenerator.randomNegOneToPosOne,
       // TODO: This halved threshold is really only relevant if sigmoid is true. Otherwise, it should be open-ended without the division.
       threshold: (numberGenerator.nextDouble / 2.0) + 0.5,
       weights: List.generate(
-          numWeights, (_) => numberGenerator.randomNegOneToPosOne),
+        numWeights,
+        (_) => (numberGenerator.randomNegOneToPosOne / weightShrinkingFactor),
+      ),
       layer: layer,
     );
   }
